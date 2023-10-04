7817d92f89d683e91d3c97585be811cfd278a244
DuctmHE153518 <97203297+DuctmHE153518@users.noreply.github.com>
2023-10-04 19:06:06 +0700

    add class controller

--- SEP_BackEnd/DataAccess/DTO/LoginDTO.cs
+++ SEP_BackEnd/DataAccess/DTO/LoginDTO.cs
@@ -9,6 +9,6 @@ namespace DataAccess.DTO
     public class LoginDTO
     {
         public string Email { get; set; }   
-        public string Password { get; set; }    
+        public string Password { get; set; }
     }
 }
---
+++ SEP_BackEnd/SEP_BackEndCodeApi/Controllers/ClassController.cs
@@ -0,0 +1,29 @@
+﻿using BussinessObject.Models;
+using DataAccess.Repositories.IReporitory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SEP_BackEndCodeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private IConfiguration _config;
+        private readonly DB_SEP490Context _db;
+        private readonly IUserRepository _user;
+
+        [HttpGet]
+        public IActionResult ClassList()
+        {
+            try
+            {
+                return Ok(_db.Classes.ToList());
+            } 
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
