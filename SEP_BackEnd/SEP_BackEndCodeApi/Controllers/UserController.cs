d15063523916538371a250811cd88fa5c246d734
Cuongcute <ngobacuong2211@gmail.com>
2023-10-05 00:58:56 +0700

    update teacher search

---
+++ SEP_BackEnd/SEP_BackEndCodeApi/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using BussinessObject.Models;
+using DataAccess.Repositories.IReporitory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEP_BackEndCodeApi.Controllers
+{
+    public class UserController : Controller
+    {
+        private IConfiguration _config;
+        private readonly DB_SEP490Context _db;
+        private readonly IUserRepository _user;
+        public UserController(IUserRepository user, IConfiguration config, DB_SEP490Context db)
+        {
+            _config = config;
+            _db = db;
+            _user = user;
+        }
+
+        [HttpGet("GetUserById/{UserID}")]
+        public IActionResult GetTeacherById(int UserID)
+        {
+            var listClass = _db.Users.Where(x => x.UserId == UserID && x.RoleId ==1).ToList();
+            if (listClass == null)
+            {
+                return NotFound();
+            }
+            return Ok(listClass);
+        }
+    }
+}
