b9496889333a78c58db87fe9e5c48a7afefaa06b
nhatvm10 <nhatvuminh00@gmail.com>
2023-10-06 23:50:23 +0700

    sửa db / làm phần data

--- SEP_BackEnd/SEP_BackEndCodeApi/Controllers/UserController.cs
+++ SEP_BackEnd/SEP_BackEndCodeApi/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using BussinessObject.Models;
+using DataAccess.DTO;
 using DataAccess.Repositories.IReporitory;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace SEP_BackEndCodeApi.Controllers
 {
@@ -8,12 +10,12 @@ namespace SEP_BackEndCodeApi.Controllers
     {
         private IConfiguration _config;
         private readonly DB_SEP490Context _db;
-        private readonly IUserRepository _user;
-        public UserController(IUserRepository user, IConfiguration config, DB_SEP490Context db)
+        private readonly IUserRepository user;
+        public UserController(IUserRepository _user, IConfiguration config, DB_SEP490Context db)
         {
             _config = config;
             _db = db;
-            _user = user;
+            user = _user;
         }
 
         [HttpGet("GetUserById/{UserID}")]
@@ -26,5 +28,82 @@ namespace SEP_BackEndCodeApi.Controllers
             }
             return Ok(listClass);
         }
+
+        public IActionResult GetAllUser()
+        {
+            var lstUser = user.GetUserList();
+            return Ok(lstUser);
+        }
+
+        [HttpGet]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+            if (user != null)
+            {
+                foreach (var item in _db.Roles.ToList())
+                {
+                    if (item.RoleId == user.RoleId)
+                    {
+                        user.Role = item;
+                    }
+                }
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetUserByEmail(string email, string fullname)
+        {
+            if (email != null)
+            {
+                var use = await _db.Users.FirstOrDefaultAsync(x => x.Email == email);
+                if (use == null)
+                {
+                    User a = new User
+                    {
+                        Email = email,
+                        FullName = fullname,
+                        RoleId = 2
+                    };
+                    _db.Users.Add(a);
+                    await _db.SaveChangesAsync();
+                    var id = await _db.Users.OrderBy(x => x.UserId).LastOrDefaultAsync();
+                    return new JsonResult(id.UserId);
+
+                }
+                return new JsonResult(use.UserId);
+            }
+            return new JsonResult("");
+        }
+
+        [HttpPost]
+        public IActionResult InsertUser(User use)
+        {
+            var a = user.GetUserByEmail(use.Email);
+            if (a != null)
+            {
+                return NotFound();
+            }
+            user.AddNew(use);
+
+            return Ok("Inserted Successfull!!!");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser(User u)
+        {
+            try
+            {
+                user.Update(u);
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
     }
 }
