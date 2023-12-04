1b5e8b587c6fb8f15bf7b76cc57b4ac0ab6f82b0
nhatvm10 <nhatvuminh00@gmail.com>
2023-12-05 00:34:19 +0700

    sửa file context

--- SEP_BackEnd/BussinessObject/Models/DB_SEP490Context.cs
+++ SEP_BackEnd/BussinessObject/Models/DB_SEP490Context.cs
@@ -47,7 +47,7 @@ namespace BussinessObject.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DUCTRAN\\SQLEXPRESS;uid=sa;pwd=2001;database=DB_SEP490;Integrated security = True; TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=sa;database=DB_SEP490;Integrated security = True; TrustServerCertificate=True;");
             }
         }
 
