56793266ab0dc8d53613ed86aa016d41cddb0d7e
DuctmHE153518 <97203297+DuctmHE153518@users.noreply.github.com>
2023-12-12 15:49:41 +0700

    update classController

--- SEP_BackEnd/SEP_BackEndCodeApi/Controllers/ClassController.cs
+++ SEP_BackEnd/SEP_BackEndCodeApi/Controllers/ClassController.cs
@@ -194,70 +194,40 @@ namespace SEP_BackEndCodeApi.Controllers
         }
 
         //danh sach lop hoc cua giao vien do, viewclass
-        [HttpGet("{userId}")]
-        public IActionResult GetClassListForRole(int userId)
+        [HttpGet]
+        public IActionResult GetClassListForTeacher(int userId)
         {
             try
             {
-                User us = _db.Users.FirstOrDefault(u => u.UserId == userId);
                 var allClass = _db.Classes.Include(t => t.Teacher).
                     Include(c => c.Course).ToList();
                 if (allClass == null)
                 {
                     return NotFound();
                 }
-                if (us.RoleId == 1)
+                var result = allClass.Where(u => u.TeacherId == userId).Select(x => new ClassDTO()
                 {
-                    var result = allClass.Where(u => u.TeacherId == userId).Select(x => new ClassDTO()
-                    {
-                        ClassId = x.ClassId,
-                        ClassName = x.ClassName,
-                        TeacherId = x.TeacherId,
-                        TeacherName = x.Teacher?.FullName,
-                        CourseId = x.CourseId,
-                        CourseName = x.Course?.CourseName,
-                        NumberStudent = x.NumberStudent,
-                        Topic = x.Topic,
-                        Schedule = x.Schedule,
-                        Fee = x.Fee,
-                        NumberOfWeek = x.NumberOfWeek,
-                        NumberPhone = x.NumberPhone,
-                        Description = x.Description,
-                        CreateDate = x.CreateDate,
-                        StartDate = x.StartDate,
-                        EndDate = x.EndDate,
-                        Status = x.Status,
-                        IsDelete = x.IsDelete,
-                        TokenClass = x.TokenClass
-                    }).ToList();
-                    return Ok(result);
-                }
-                else
-                {
-                    var result = allClass.Select(x => new ClassDTO()
-                    {
-                        ClassId = x.ClassId,
-                        ClassName = x.ClassName,
-                        TeacherId = x.TeacherId,
-                        TeacherName = x.Teacher?.FullName,
-                        CourseId = x.CourseId,
-                        CourseName = x.Course?.CourseName,
-                        NumberStudent = x.NumberStudent,
-                        Topic = x.Topic,
-                        Schedule = x.Schedule,
-                        Fee = x.Fee,
-                        NumberOfWeek = x.NumberOfWeek,
-                        NumberPhone = x.NumberPhone,
-                        Description = x.Description,
-                        CreateDate = x.CreateDate,
-                        StartDate = x.StartDate,
-                        EndDate = x.EndDate,
-                        Status = x.Status,
-                        IsDelete = x.IsDelete,
-                        TokenClass = x.TokenClass
-                    }).ToList();
-                    return Ok(result);
-                }
+                    ClassId = x.ClassId,
+                    ClassName = x.ClassName,
+                    TeacherId = x.TeacherId,
+                    TeacherName = x.Teacher?.FullName,
+                    CourseId = x.CourseId,
+                    CourseName = x.Course?.CourseName,
+                    NumberStudent = x.NumberStudent,
+                    Topic = x.Topic,
+                    Schedule = x.Schedule,
+                    Fee = x.Fee,
+                    NumberOfWeek = x.NumberOfWeek,
+                    NumberPhone = x.NumberPhone,
+                    Description = x.Description,
+                    CreateDate = x.CreateDate,
+                    StartDate = x.StartDate,
+                    EndDate = x.EndDate,
+                    Status = x.Status,
+                    IsDelete = x.IsDelete,
+                    TokenClass = x.TokenClass
+                }).ToList();
+                return Ok(result);
             }
             catch (Exception ex)
             {
@@ -271,46 +241,34 @@ namespace SEP_BackEndCodeApi.Controllers
         {
             try
             {
-                Class cl = _db.Classes.FirstOrDefault(cla => cla.ClassId == classId);
-                if (cl.TeacherId == null)
+                var allClass = _db.Classes.Include(t => t.Teacher).Include(c => c.Course).ToList();
+                if (allClass == null)
                 {
-                    var allClass = _db.Classes.Include(c => c.Course).ToList();
-                    if (allClass == null)
-                    {
-                        return NotFound();
-                    }
-                    var result = allClass.Where(cd => cd.ClassId == classId).Select(x => new ClassDTO()
-                    {
-                        ClassId = x.ClassId, ClassName = x.ClassName,
-                        CourseId = x.CourseId, CourseName = x.Course.CourseName,
-                        Topic = x.Topic, Schedule = x.Schedule, Fee = x.Fee,
-                        NumberOfWeek = x.NumberOfWeek, NumberPhone = x.NumberPhone,
-                        Description = x.Description, CreateDate = x.CreateDate,
-                        Status = x.Status, IsDelete = x.IsDelete, TokenClass = x.TokenClass
-                    }).FirstOrDefault();
-                    return Ok(result);
+                    return NotFound();
                 }
-                else
+                var result = allClass.Where(cd => cd.ClassId == classId).Select(x => new ClassDTO()
                 {
-                    var allClass = _db.Classes.Include(t => t.Teacher).Include(c => c.Course).ToList();
-                    if (allClass == null)
-                    {
-                        return NotFound();
-                    }
-                    var result = allClass.Where(cd => cd.ClassId == classId).Select(x => new ClassDTO()
-                    {
-                        ClassId = x.ClassId, ClassName = x.ClassName,
-                        TeacherId = x.TeacherId, TeacherName = x.Teacher.FullName,
-                        CourseId = x.CourseId, CourseName = x.Course.CourseName,
-                        NumberStudent = x.NumberStudent, Topic = x.Topic,
-                        Schedule = x.Schedule, Fee = x.Fee, NumberOfWeek = x.NumberOfWeek, 
-                        NumberPhone = x.NumberPhone, Description = x.Description, 
-                        CreateDate = x.CreateDate, StartDate = x.StartDate, 
-                        EndDate = x.EndDate, Status = x.Status, IsDelete = x.IsDelete, 
-                        TokenClass = x.TokenClass
-                    }).FirstOrDefault();
-                    return Ok(result);
-                }
+                    ClassId = x.ClassId,
+                    ClassName = x.ClassName,
+                    TeacherId = x.TeacherId,
+                    TeacherName = x.Teacher.FullName,
+                    CourseId = x.CourseId,
+                    CourseName = x.Course.CourseName,
+                    NumberStudent = x.NumberStudent,
+                    Topic = x.Topic,
+                    Schedule = x.Schedule,
+                    Fee = x.Fee,
+                    NumberOfWeek = x.NumberOfWeek,
+                    NumberPhone = x.NumberPhone,
+                    Description = x.Description,
+                    CreateDate = x.CreateDate,
+                    StartDate = x.StartDate,
+                    EndDate = x.EndDate,
+                    Status = x.Status,
+                    IsDelete = x.IsDelete,
+                    TokenClass = x.TokenClass
+                }).FirstOrDefault();
+                return Ok(result);
             }
             catch (Exception ex)
             {
